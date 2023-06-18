e11d64709ecea320a04afa01c758d0356d042ae1
leonooo13 <leonooo13@qq.com>
2023-06-18 19:21:35 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+author: leonooo13
+date: 18/06/2023
+Some Scripties aim to improve efficiency of production
--- ReEmpytFloder.py
+++ ReEmpytFloder.py
@@ -1,6 +1,9 @@
 import os
 current_floder="E:\Algorithms"
 def delete_empty(current_floder:str):
+    if len(current_floder)==0:
+        current_floder=os.getcwd()
+    print("current path is ",current_floder)
     for root,dir,filename in os.walk(current_floder):
         for dirname in dir:
             dirpath=os.path.join(root,dirname)
@@ -15,5 +18,7 @@ if __name__=="__main__":
     print("*"*10)
     print("input the floder which is you want to delete empty floder")
     print("for example: | E:\Algorithms| it will delete all empty floders automatically")
+    print("if you willn't to keyin you need it place it target dir blow,This way you entry Enter")
+    print("*"*10)
     floder=input()
     delete_empty(floder)
